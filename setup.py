329aa4049031f41013d849af1c82ff6336772cdc
Joakim Uddholm <joakim@uddholm.com>
2017-07-09 03:50:56 +0200

    Ready for release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.command.install_data import install_data
 
 from setuptools import setup, find_packages
 
-VERSION = "0.0.1"
+VERSION = "1.0.0"
 with open("LICENSE") as f:
     LICENSE = f.read()
 with open("README.md") as f:
@@ -13,11 +13,11 @@ setup(
         version=VERSION,
         author='Joakim Uddholm',
         author_email='tethik@gmail.com',
-        description='Simple numeric captcha implementation for flask and flask-session.',
+        description='Captcha implementation for flask and flask-session.',
         long_description=README,
-        url='https://github.com/Tethik/another-flask-captcha',
+        url='https://github.com/Tethik/flask-session-captcha',
         packages=['flask_session_captcha'],
-        package_data={'': ['LICENSE', 'README.md']},
+        package_data={'': ['LICENSE', 'README.md', 'DEVELOPMENT.md']},
         install_requires=[
             'captcha',
             'flask-session',
@@ -26,6 +26,23 @@ setup(
         tests_require=[
             'pytest',
             'pytest-cov',
+            'flask-sqlalchemy',
+        ],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Operating System :: OS Independent',
+            'Programming Language :: Python',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+            'Topic :: Software Development :: Libraries :: Python Modules',            
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Security',
         ],
         license=LICENSE
 )
