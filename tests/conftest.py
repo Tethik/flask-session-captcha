d2f7963bec3d1ff6d977328ddfc7a0a3a9b30a4f
ali sharifi <alisharifyofficial@gmail.com>
2024-02-25 15:09:00 +0330

    finish testing
    testing funcs:
    - test_captcha_length
    - test_captcha_correct_answer
    - test_captcha_wrong_answer
    - test_captcha_render_in_template
    - test_captcha_enable
    - test_captcha_config_set_ok

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,17 @@
 import os
+
 import pytest
-from flask import Flask
-from flask_session_captcha import FlaskSessionCaptcha
+from flask import Flask, render_template
 from flask_session import Session
+
+from flask_session_captcha import FlaskSessionCaptcha
+
 
 @pytest.fixture()
 def app():
+    """
+    Creating Flask Application + adding base config for flask captcha session packages
+    """
     app = Flask(__name__)
     app.config.update({
         'TESTING': True,
@@ -15,23 +21,45 @@ def app():
 
     # captcha configs:
     app.config['CAPTCHA_ENABLE'] = True
-    app.config['CAPTCHA_LENGTH'] = 5
+    app.config['CAPTCHA_LOG'] = False
+    app.config['CAPTCHA_LENGTH'] = 4
     app.config['CAPTCHA_WIDTH'] = 200
     app.config['CAPTCHA_HEIGHT'] = 160
     app.config['CAPTCHA_INCLUDE_NUMERIC'] = True
     app.config['CAPTCHA_INCLUDE_ALPHABET'] = True
     app.config['CAPTCHA_INCLUDE_PUNCTUATION'] = False
     app.config['CAPTCHA_SESSION_KEY'] = 'test_captcha_image'
+
     # session config
-    app.config['SESSION_TYPE'] = 'redis'
+    app.config['SESSION_TYPE'] = 'filesystem'
     Session(app)
 
-    captcha_object = FlaskSessionCaptcha(app=app)
-    app.extensions['C'] = captcha_object
+    captcha = FlaskSessionCaptcha(app=app)
 
     yield app
+
 
 @pytest.fixture()
 def client(app):
     yield app.test_client()
 
+
+@pytest.fixture()
+def captcha(app):
+    yield app.extensions['flask_session_captcha']
+
+
+@pytest.fixture()
+def bind_base_views(app, captcha):
+    """adding base views to flask application for testing captcha validate method"""
+
+    @app.get("/test/")
+    def index_get():
+        return render_template("index.html")
+
+    @app.post("/test/")
+    def index_post():
+        if captcha.validate():
+            return "OK"
+        else:
+            return "NOT OK"
